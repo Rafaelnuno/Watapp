291edf58073e5ed432e5b89ac27df9c54c765fa7
Rafael Nuno de Freitas Pilré <rafel.nuno@gmail.com>
2020-11-22 19:01:15 +0000

    Mover a pasta code e docs

---
+++ code/src/main/java/pt/iade/Watapp/WatappApplication.java
@@ -0,0 +1,13 @@
+package pt.iade.Watapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatappApplication.class, args);
+	}
+
+}
